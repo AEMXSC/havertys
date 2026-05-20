6261b4c46ee5bbb91479bfa17fa60c78622431a7
AEMXSC <aemxsc@adobe.com>
2026-05-20 02:55:32 +0000

    Apply background images from DA data attributes on sections

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -447,6 +447,17 @@ export function decorateMain(main) {
   decorateIcons(main);
   buildAutoBlocks(main);
   decorateSections(main);
+
+  // Apply background images from DA data attributes (DA converts section-metadata to data attrs)
+  main.querySelectorAll('.section[data-backgroundimage]').forEach((section) => {
+    const bgImg = section.dataset.backgroundimage;
+    if (bgImg) {
+      section.style.backgroundImage = `url(${bgImg})`;
+      section.style.backgroundSize = 'cover';
+      section.style.backgroundPosition = 'center center';
+    }
+  });
+
   decorateBlocks(main);
 }
 
